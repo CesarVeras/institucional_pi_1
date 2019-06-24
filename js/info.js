69ce48a4a80ed8c1eff6d1c1732f86a8526bb7cf
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2019-06-24 19:38:02 -0300

    Ajuste no script da página de informações

--- js/info.js
+++ js/info.js
@@ -1,10 +1,15 @@
-const extra_heigh = 93.8 + 112;
+const header = document.querySelector('header');
+const header_size = parseFloat(getComputedStyle(header).getPropertyValue('height'));
+const footer = document.querySelector('footer');
+const footer_size = parseFloat(getComputedStyle(footer).getPropertyValue('height'));
+
+const extra_heigh = header_size + footer_size;
 const window_height = window.innerHeight;
 
 // Verifica se o tamanho do conteudo é menor que o tamanho disponível da tela, se sim, aumenta o tamanho do corpo para preencher melhor.
 document.body.onload = () => {
     // altura da janela do browser
-    let height = parseInt(getComputedStyle(document.body).getPropertyValue('height'),10);
+    let height = parseInt(getComputedStyle(document.body).getPropertyValue('height'), 10);
     if (height < window_height) {
         document.querySelector('section').style.height = (window_height - extra_heigh) + 'px';
     }
